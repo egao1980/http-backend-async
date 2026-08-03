6c70edfb3c72333d6cb5b57fb46f93bcb8c56b5e
Nikolai Matiushev <egao1980@gmail.com>
2026-08-03 23:02:38 +0100

    Bump to 0.1.1 and packager 0.10.0 for OCI publish.
    Aligns with http-protocol 0.2.0 consumers; publish workflow defaults
    now match the release tag we will dispatch.

--- scripts/publish-source.lisp
+++ scripts/publish-source.lisp
@@ -25,7 +25,7 @@
                         out)))))))
 
 (let* ((name "http-backend-async")
-       (version (env "PKG_VERSION" "0.1.0"))
+       (version (env "PKG_VERSION" "0.1.1"))
        (source-dir (uiop:ensure-directory-pathname
                     (env "PKG_SOURCE_DIR" (namestring (uiop:getcwd)))))
        (_ (sync-asdf-version! source-dir version))
